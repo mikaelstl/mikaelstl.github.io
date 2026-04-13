3639206d712571f3541ceaceaa940fd598314c26
mikael <mikael.st15@protonmail.com>
2026-04-13 18:20:14 -0300

    init: finished project setup

---
+++ src/assets/Palette.ts
@@ -0,0 +1,26 @@
+interface Colors {
+  readonly white: string;
+  readonly black: string;
+  readonly transparent: string;
+  
+  readonly background: string;
+  readonly card: string;
+  readonly items: string;
+  readonly comic_icon: string;
+  readonly subtitles: string;
+  readonly red: string;
+  readonly light_red: string;
+}
+
+export const Palette: Colors = {
+  white: '#FFFFFF',
+  black: '#000000',
+  transparent: 'transparent',
+  background: '#0B0A0F',
+  card: '#14121B',
+  items: '#1D1B27',
+  comic_icon: '#322E43',
+  subtitles: '#6A6586',
+  red: '#E4193B',
+  light_red: '#BC3B4C'
+}
